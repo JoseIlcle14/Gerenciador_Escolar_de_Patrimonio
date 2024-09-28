d1030f8cd872d4f325b7b58f280a982d86206b13
unknown <victorgab0802@hmail.com>
2024-09-28 16:59:04 -0300

    importei o flask login

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_login import login_manager
 from routes.admin import admin_route
 
 app = Flask(__name__)
@@ -11,3 +12,4 @@ def index():
     return render_template('index.html')
 
 app.run(debug=True)
+login_manager = login_manager()
