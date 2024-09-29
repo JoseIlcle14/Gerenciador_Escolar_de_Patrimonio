6fa3f1f4f4464d698b26a633c21fc3d18ab8b315
Jose Ilcle <joseilclemaia@gmail.com>
2024-09-29 09:09:28 -0300

    banco de dados conectado corretamente

--- database/BancoGEP.py
+++ database/BancoGEP.py
@@ -1,11 +1,9 @@
 import psycopg2
 
 conexao = psycopg2.connect(
-    database = "Gerenciador_Escolar_de_Patrimonio",
+    database = "gepdb",
     host = "aws-0-us-east-1.pooler.supabase.com",
     user = "postgres.rzejcodwpgvohhskmyqm",
     password = "gerenciador123",
     port = "6543"
 )
-#Nao está funcionando ainda
-print(conexao.info)
