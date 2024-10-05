db4dc9b1eb86fd335ac75c53ebf33f13a951e40b
patluc15 <belinhalg15@gmail.com>
2024-10-05 16:07:20 -0300

    função detalhar completa

--- database/database.py
+++ database/database.py
@@ -24,46 +24,46 @@ MESA = [
 
 LOUSA = [
     
-        {'id': 1, 'cor': 'azul', 'local':'infor1', 'material':'madeira', 'movel':'CADEIRA'},
-        {'id': 2, 'cor': 'azul', 'local':'infor1', 'material':'madeira', 'movel':'CADEIRA'},
-        {'id': 3, 'cor': 'azul', 'local':'infor2', 'material':'madeira', 'movel':'CADEIRA'},
-        {'id': 4, 'cor': 'cinza', 'local':'infor1', 'material':'madeira', 'movel':'CADEIRA'},
-        {'id': 5, 'cor': 'azul', 'local':'infor3', 'material':'madeira', 'movel':'CADEIRA'},
-        {'id': 6, 'cor': 'azul', 'local':'infor2', 'material':'madeira', 'movel':'CADEIRA'}
+        {'id': 1, 'cor': 'azul', 'local':'infor1', 'material':'madeira', 'movel':'LOUSA'},
+        {'id': 2, 'cor': 'azul', 'local':'infor1', 'material':'madeira', 'movel':'LOUSA'},
+        {'id': 3, 'cor': 'azul', 'local':'infor2', 'material':'madeira', 'movel':'LOUSA'},
+        {'id': 4, 'cor': 'cinza', 'local':'infor1', 'material':'madeira', 'movel':'LOUSA'},
+        {'id': 5, 'cor': 'azul', 'local':'infor3', 'material':'madeira', 'movel':'LOUSA'},
+        {'id': 6, 'cor': 'azul', 'local':'infor2', 'material':'madeira', 'movel':'LOUSA'}
 
 ]
 
  
 AR = [
 
-        {'id': 1, 'cor': 'azul', 'local':'infor1', 'material':'ferro'},
-        {'id': 2, 'cor': 'azul', 'local':'infor1', 'material':'ferro'},
-        {'id': 3, 'cor': 'azul', 'local':'infor2', 'material':'ferro'},
-        {'id': 4, 'cor': 'cinza', 'local':'infor1', 'material':'ferro'},
-        {'id': 5, 'cor': 'azul', 'local':'infor3', 'material':'ferro'},
-        {'id': 6, 'cor': 'azul', 'local':'infor2', 'material':'ferro'}
+        {'id': 1, 'cor': 'azul', 'local':'infor1', 'material':'ferro', 'movel':'AR'},
+        {'id': 2, 'cor': 'azul', 'local':'infor1', 'material':'ferro', 'movel':'AR'},
+        {'id': 3, 'cor': 'azul', 'local':'infor2', 'material':'ferro', 'movel':'AR'},
+        {'id': 4, 'cor': 'cinza', 'local':'infor1', 'material':'ferro', 'movel':'AR'},
+        {'id': 5, 'cor': 'azul', 'local':'infor3', 'material':'ferro', 'movel':'AR'},
+        {'id': 6, 'cor': 'azul', 'local':'infor2', 'material':'ferro', 'movel':'AR'}
 ]
         
 
 
 PROJETOR = [
 
-        {'id': 1, 'cor': 'azul', 'local':'infor1', 'material':'madeira'},
-        {'id': 2, 'cor': 'azul', 'local':'infor1', 'material':'madeira'},
-        {'id': 3, 'cor': 'azul', 'local':'infor2', 'material':'madeira'},
-        {'id': 4, 'cor': 'cinza', 'local':'infor1', 'material':'madeira'},
-        {'id': 5, 'cor': 'azul', 'local':'infor3', 'material':'madeira'},
-        {'id': 6, 'cor': 'azul', 'local':'infor2', 'material':'madeira'}
+        {'id': 1, 'cor': 'azul', 'local':'infor1', 'material':'madeira', 'movel':'PROJETOR'},
+        {'id': 2, 'cor': 'azul', 'local':'infor1', 'material':'madeira', 'movel':'PROJETOR'},
+        {'id': 3, 'cor': 'azul', 'local':'infor2', 'material':'madeira', 'movel':'PROJETOR'},
+        {'id': 4, 'cor': 'cinza', 'local':'infor1', 'material':'madeira', 'movel':'PROJETOA'},
+        {'id': 5, 'cor': 'azul', 'local':'infor3', 'material':'madeira', 'movel':'PROJETOR'},
+        {'id': 6, 'cor': 'azul', 'local':'infor2', 'material':'madeira', 'movel':'PROJETOR'}
 
 ]
 
 VENTILADOR = [
     
-        {'id': 1, 'cor': 'azul', 'local':'infor1', 'material':'madeira'},
-        {'id': 2, 'cor': 'azul', 'local':'infor1', 'material':'madeira'},
-        {'id': 3, 'cor': 'azul', 'local':'infor2', 'material':'madeira'},
-        {'id': 4, 'cor': 'cinza', 'local':'infor1', 'material':'madeira'},
-        {'id': 5, 'cor': 'azul', 'local':'infor3', 'material':'madeira'},
-        {'id': 6, 'cor': 'azul', 'local':'infor2', 'material':'madeira'}
+        {'id': 1, 'cor': 'azul', 'local':'infor1', 'material':'madeira', 'movel':'VENTILADOR'},
+        {'id': 2, 'cor': 'azul', 'local':'infor1', 'material':'madeira', 'movel':'VENTILADOR'},
+        {'id': 3, 'cor': 'azul', 'local':'infor2', 'material':'madeira', 'movel':'VENTILADOR'},
+        {'id': 4, 'cor': 'cinza', 'local':'infor1', 'material':'madeira', 'movel':'VENTILADOR'},
+        {'id': 5, 'cor': 'azul', 'local':'infor3', 'material':'madeira', 'movel':'VENTILADOR'},
+        {'id': 6, 'cor': 'azul', 'local':'infor2', 'material':'madeira', 'movel':'VENTILADOR'}
 
 ]
