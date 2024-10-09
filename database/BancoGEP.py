67ccaf743d4c5a60e864c66c4b81bfd3c26b933a
Jose Ilcle <joseilclemaia@gmail.com>
2024-10-09 05:27:55 -0300

    comandos de inserir dados

--- database/BancoGEP.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-conexao = psycopg2.connect(
-    database = "gepdb",
-    host = "aws-0-us-east-1.pooler.supabase.com",
-    user = "postgres.rzejcodwpgvohhskmyqm",
-    password = "gerenciador123",
-    port = "6543"
-)
--- database/GEPdb.py
+++ database/GEPdb.py
@@ -9,36 +9,16 @@ conexao = mysql.connector.connect(
 cursor = conexao.cursor()
 
 
-def inserir_usuarios():
-    
-    comando = f'insert into usuarios (login,senha,tipo) values("{login}","{senha}","{tipo}")'
-    cursor.execute(comando)
-    conexao.commit()
-
-def inserir_moveis():
-    # id,nome,cor,material,sala
-    comando = f'insert into moveis (nome,cor,material,sala) values("{nome}","{cor}","{material}","{sala}")'
-    cursor.execute(comando)
-    conexao.commit()
-    
-
-def inserir_eletronicos():
-    comando = f'insert into eletronicos (nome,consumoEnergia) values("{nome}","{consumoEnergia}")'
-    cursor.execute(comando)
-    conexao.commit()
-
 def inserir_objetos():
-    comp = input("Você deseja inserir um movel(m) ou um eletrônico(e)?")
-    if comp == 'm':
-        inserir_moveis()
-    if comp == 'e':
-        inserir_eletronicos()
+    objeto = input("Informe o objeto de inserção: ")
+    local = int(input("diga o numero da sala: "))
+    id = 33101
+    if objeto == 'cadeira' or objeto == 'mesa' and local == 1 :
+        for i in range (46):
+            comando = f'insert into {objeto}(id,cor,material,idsala) values({id},"azul","ferro e plástico",{local})'
+            id += 1
 
-
-inserir_usuarios()
 inserir_objetos()
-
-
 #resultado = cursor.fetchall() #lê o banco de dados
 cursor.close()
 conexao.close()
