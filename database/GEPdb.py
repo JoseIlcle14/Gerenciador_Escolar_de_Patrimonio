8bc7fbee3fa2debf747ff2b23db796630016d020
Jose Ilcle <joseilclemaia@gmail.com>
2024-10-12 09:12:17 -0300

    preencimento banco de dados

--- database/GEPdb.py
+++ database/GEPdb.py
@@ -8,17 +8,11 @@ conexao = mysql.connector.connect(
 
 cursor = conexao.cursor()
 
-
-def inserir_objetos():
-    objeto = input("Informe o objeto de inserção: ")
-    local = int(input("diga o numero da sala: "))
-    id = 33101
-    if objeto == 'cadeira' or objeto == 'mesa' and local == 1 :
-        for i in range (46):
-            comando = f'insert into {objeto}(id,cor,material,idsala) values({id},"azul","ferro e plástico",{local})'
-            id += 1
-
 inserir_objetos()
+
+#cursor.execute(comando)
+#conexao.commit()
+    
 #resultado = cursor.fetchall() #lê o banco de dados
 cursor.close()
 conexao.close()
