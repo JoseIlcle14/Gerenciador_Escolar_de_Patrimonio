3fea894780f21305f82cae7b4e1a9b809c17659d
Fco Kauan <fcokauanclash@gmail.com>
2024-10-13 23:51:48 -0300

    alterações para a versão mobile na navMenu

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     $('#mobile--btn').on('click', function(){
         $('#mobile--menu').toggleClass('active');
-        $('#mobile--btn').find('i').toggleClass('fa-x');
+        // $('#mobile--btn').find('i').toggleClass('fa-x');
     });
 });
+
+$(document).ready(function(){
+    $('#mobile--btn').on('click', function(){
+        $('#mobile--btn').find('i').toggleClass('fa-x');
+    })
+})
