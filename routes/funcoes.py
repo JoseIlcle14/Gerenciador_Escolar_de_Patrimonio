a13d03ba6f5c0041159f5df5ace8ee43ce3e980d
unknown <victorgab0802@hmail.com>
2024-11-23 11:56:02 -0300

    nada

--- routes/funcoes.py
+++ routes/funcoes.py
@@ -112,9 +112,9 @@ def editar_item(item_id):
     if tabela_moveis:
         
         item =  db.session.query(Moveis).filter_by(id=item_id).first()
-        item.id_sala = request.form['turma']    
-        item.material = request.form['material']
-        item.cor = request.form['cor']
+        # item.id_sala = request.form['turma']    
+        # item.material = request.form['material']
+        # item.cor = request.form['cor']
         
         return redirect(f'{id_obj}')
     else:
